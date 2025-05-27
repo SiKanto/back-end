6f70af7612f4d239d2a67959b84733fd329393aa
X_Raxs <113223781+XRaxs@users.noreply.github.com>
2025-05-28 02:17:14 +0700

    login register aman

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -46,5 +47,20 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Menambahkan method untuk mencocokkan password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Enkripsi password sebelum menyimpannya ke database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
